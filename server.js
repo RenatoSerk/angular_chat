6fa8629c8da23aff23c917b0a4c4cc4d56eaa2b9
RenatoSerk <brickworked@gmail.com>
2020-09-10 11:10:32 -0400

    Change app to work when not using localhost
    Angular front-end is now served from a directory by the node server when hosted, no longer requiring two servers when hosted on heroku

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 //require('dotenv').config();
+const express = require('express');
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const path = require('path');
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('hello!'));
+app.use(express.static(path.join(__dirname, 'dist/socket-frontend')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/socket-frontend/index.html'))
+});
 
 io.on('connection', (socket) => {
     console.log('a user connected');
